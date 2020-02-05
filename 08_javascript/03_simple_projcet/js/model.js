0d2fd8b539ca3dc1ef34b3aa917cf8d1dc64f475
Mehdi <msoli666@gmail.com>
2020-02-05 09:34:41 +0330

    Start to writing model.js file to store, get and delete data

--- 08_javascript/03_simple_projcet/js/model.js
+++ 08_javascript/03_simple_projcet/js/model.js
@@ -1,3 +1,47 @@
 /**
  * Model file for working with data
  * */
+
+/**
+ * Main Model Object
+ * */
+
+/**
+ *
+ * @type {{}}
+ */
+let model = {};
+
+// noinspection JSAnnotator
+/**
+ * Gets posts from local store
+ *
+ * @return posts {array} An array of post objects
+ */
+model.getPost = function () {
+
+};
+
+
+
+/**
+ * Gets content from local store
+ *
+ * @return store {object} object or array of object of site data
+ */
+
+
+/**
+ * Save temporary store to local storage
+ *
+ * @param store {string} JSON string of data to store
+ */
+
+model.updateLocalStore = function (store) {
+    localStorage.setItem('gholamPress', store);
+};
+
+/**
+ * Delete data from local store
+ *
+ */
