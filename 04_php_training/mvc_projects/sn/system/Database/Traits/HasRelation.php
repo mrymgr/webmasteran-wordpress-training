988dc32154fd3fabe3828efed56dd713361e15cc
Mehdi Soltani <msoli666@gmail.com>
2021-12-22 09:55:16 +0330

    Start to write HasRelation trait

--- 04_php_training/mvc_projects/sn/system/Database/Traits/HasRelation.php
+++ 04_php_training/mvc_projects/sn/system/Database/Traits/HasRelation.php
@@ -1,6 +1,35 @@
 <?php
+
 namespace System\Database\Traits;
 
-trait HasRelation {
+use System\Database\Traits\HasQueryBuilder;
 
+trait HasRelation
+{
+  use HasQueryBuilder;
+  protected function hasOne($model, $foreignKey, $localKey)
+  {
+    if ($this->{$this->primaryKey}) {
+      $modelObject = new $model;
+      return $modelObject->getHasOneRelation($this->table, $foreignKey, $localKey, $this->$localKey);
+    }
+  }
+  
+  protected function getHasOneRelation($table, $foreignKey, $otherKey, $otherKeyValue)
+  {
+    // sql = 'SELECT phones.* FROM users JOIN on user.id = phone.user_id'
+    $this->setSql("SELECT `b`.* FROM {$table} AS `a` JOIN {$this->getTableName()} AS `b` ON `a`.`{$otherKey}` = `b`.`{$foreignKey}` ");
+    $this->setWhere('AND', "`a`.`{$otherKey}` = ? ");
+    $this->table = 'b';
+    $this->addValue($otherKey, $otherKeyValue);
+    
+    $statement = $this->executeQuery();
+    $data = $statement->fetch();
+    if ($data) {
+      return $this->arrayToAttributes($data);
+    }
+    return null;
+    
+  }
+  
 }
